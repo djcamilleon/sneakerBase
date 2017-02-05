b9c7177c59a71e97a8dc069eaf7893e8e07b86b3
Corey Harville <coreyharvillewebdev@gmail.com>
2017-02-04 18:18:01 -0700

    Add New Shoe view progress

--- public/js/controllers/singleShoeCtrl.js
+++ public/js/controllers/singleShoeCtrl.js
@@ -1,5 +1,6 @@
 angular.module('sneakerBase').controller('singleShoeCtrl', function($scope, $state, mainSvc) {
     $scope.shoe = mainSvc.shoe;
     $scope.photos = mainSvc.shoe.photos
-    console.log($scope.photos);
+        // console.log($scope.photos);
+    console.log($scope.shoe);
 });

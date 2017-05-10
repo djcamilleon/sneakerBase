48f29137a51fb9dd179a85207fd16df91d4907a3
Corey Harville <coreyharvillewebdev@gmail.com>
2017-05-09 20:53:47 -0600

    Added Retired and Sold Features

--- public/js/controllers/newShoeCtrl.js
+++ public/js/controllers/newShoeCtrl.js
@@ -1,22 +1,22 @@
 angular.module('sneakerBase')
   .controller('newShoeCtrl', function ($scope, mainSvc) {
-    
+
     $scope.newShoeFeatures = {
-      features: [{text: ''}],
-      links: [{text: ''}],
-      photos: [{text: ''}]
-    };
-    
-    $scope.addFeature = function() {
-      $scope.newShoeFeatures.features.push({text: ''});
+      features: [{ text: '' }],
+      links: [{ text: '' }],
+      photos: [{ text: '' }]
     };
 
-    $scope.addLinks = function() {
-      $scope.newShoeFeatures.links.push({text: ''});
+    $scope.addFeature = function () {
+      $scope.newShoeFeatures.features.push({ text: '' });
     };
 
-    $scope.addPhotos = function() {
-      $scope.newShoeFeatures.photos.push({text: ''});
+    $scope.addLinks = function () {
+      $scope.newShoeFeatures.links.push({ text: '' });
+    };
+
+    $scope.addPhotos = function () {
+      $scope.newShoeFeatures.photos.push({ text: '' });
     };
 
     $scope.registerNewShoe = function (registerObj) {
@@ -26,23 +26,5 @@ angular.module('sneakerBase')
         // console.log(shoe_id)
         $scope.shoe_id = shoe_id.data[0].id;
       })
-
-      
-
-    //   $scope.registerNewShoeDetails = function(f1, f2, f3) {
-    //     var obj = {
-    //       photos: [],
-    //       features: [f1, f2, f3]
-    //     }
-    //     // console.log(obj);
-    //     // console.log('Same shoe_id', $scope.shoe_id)
-    //   mainSvc.registerNewShoeDetails(obj, $scope.shoe_id).then(function(obj){
-    //     // console.log('This should be the detailsObj from newShoeCtrl', detailsObj);
-    //     // console.log('This is the shoe_id from newShoeCtrl.', $scope.shoe_id)
-    //     console.log('Returned obj after post', obj);
-    //   })
-    // }
-
     };
-    
-})
+  })

ea7c31bdce2115ad9cff6a88ac84c6e0998b19e5
Corey Harville <coreyharvillewebdev@gmail.com>
2019-10-10 22:45:35 -0600

    Updated Sschema.sql

--- schema.sql
+++ schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE shoes (
   id SERIAL PRIMARY KEY,
   brand VARCHAR(255),
   display_model VARCHAR(255),
-  sort_model VARCHAR(255),
   nickname VARCHAR(255),
   colorway VARCHAR(255),
+  sort_model VARCHAR(255),
   primary_color VARCHAR(255),
   accent_color1 VARCHAR(255),
   accent_color2 VARCHAR(255),
@@ -23,7 +23,7 @@ CREATE TABLE shoes (
   user_id INTEGER
 );
 
-insert into shoes (brand, display_model, sort_model, nickname, colorway, primary_color, accent_color1, accent_color2, link_nickname, style_code, size, designer, model_details, colorway_details, release_date,price, associated_athlete, forefoot_cushioning_technology, heel_cushioning_technology, type, user_id) 
+insert into shoes (brand, display_model, nickname, colorway, sort_model, primary_color, accent_color1, accent_color2, link_nickname, style_code, size, designer, model_details, colorway_details, release_date,price, associated_athlete, forefoot_cushioning_technology, heel_cushioning_technology, type, user_id) 
 values
 
 CREATE TABLE features (
